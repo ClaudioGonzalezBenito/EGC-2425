1bbdb097ebee5e0ceac4c8c5d07126830cc9299f
ClaudioGonzalezBenito <clagonben@alum.us.es>
2025-01-10 17:15:42 +0100

    test:wf probar dependecias desactulizadas python3

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ migrate = Migrate()
 def create_app(config_name='development'):
     app = Flask(__name__)
 
-    # Load configuration according to environmentt
+    # Load configuration according to environment
     config_manager = ConfigManager(app)
     config_manager.load_config(config_name=config_name)
 
